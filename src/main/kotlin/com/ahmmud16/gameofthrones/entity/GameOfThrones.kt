9af2ccd3e5642a5d7a58f4c67a2ba3ba9e46806a
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-10-02 18:20:01 +0200

    started creating entity

--- src/main/kotlin/com/ahmmud16/gameofthrones/entity/GameOfThrones.kt
+++ src/main/kotlin/com/ahmmud16/gameofthrones/entity/GameOfThrones.kt
@@ -1,9 +1,51 @@
 package com.ahmmud16.gameofthrones.entity
 
+import javax.persistence.ElementCollection
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 
 @Entity
-class GameOfThrones {
+class GameOfThrones (
 
 
-}
+        @get:NotBlank
+        @get:Size(max = 128)
+        var characterName: String,
+
+        @ElementCollection
+        @get:NotNull
+        var houseName: Set<String>? = setOf(),
+
+        @get:NotBlank
+        @get:Size(max = 2048)
+        var characterImageThumb: String? = null,
+
+        @get:NotBlank
+        @get:Size(max = 2048)
+        var characterImageFull: String? = null,
+
+        @get:ElementCollection
+        @get:NotBlank
+        var parents: Set<String>? = setOf(),
+
+        @get:ElementCollection
+        @get:NotBlank
+        var parentsOf: Set<String>? = setOf(),
+
+        @get:NotBlank
+        var royal: Boolean? = null,
+
+        @get:ElementCollection
+        @get:NotNull
+        var killed: Set<String>? = setOf(),
+
+        @get:ElementCollection
+
+        @get:Id
+        @get:GeneratedValue
+        var id: Long? = null
+)
