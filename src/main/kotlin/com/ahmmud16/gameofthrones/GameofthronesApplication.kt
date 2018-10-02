10aaf2ec762e8fd8cf97868a62cf38bf6aaf4adc
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-10-02 16:04:21 +0200

    uploading project

---
+++ src/main/kotlin/com/ahmmud16/gameofthrones/GameofthronesApplication.kt
@@ -0,0 +1,11 @@
+package com.ahmmud16.gameofthrones
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GameofthronesApplication
+
+fun main(args: Array<String>) {
+    runApplication<GameofthronesApplication>(*args)
+}
